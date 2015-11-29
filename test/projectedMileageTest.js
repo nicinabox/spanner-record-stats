0d94bf97047da718020c2477fc7e9ce287252061
Nic Aitch <nic@nicinabox.com>
2015-11-29 11:44:06 -0600

    Reorganize

--- test/projectedMileageTest.js
+++ test/projectedMileageTest.js
@@ -6,34 +6,20 @@ import rawMiniRecords from '../data/mini.json'
 import rawGuzziRecords from '../data/guzzi.json'
 import rawSuzukiRecords from '../data/suzuki.json'
 
-var lastSeason = compose(partialRight(takeRight, 4), milesPerDaySet)
-
-var sortRecords = (records) => {
-  return sortBy(reject(records, ({mileage}) => !mileage), 'mileage')
-}
-
-var percent = (n, p) => {
-  return n * (p / 100)
-}
-
-var bounds = (n, variance) => {
-  return n + variance
-}
-
-var variance = partialRight(percent, 1)
-
-var lower = (mi) => bounds(mi, -variance(mi))
-var upper = (mi) => bounds(mi, variance(mi))
-
-var cpm = (records) => {
-  return currentProjectedMileage(lastSeason(records), last(records))
-}
+var sortRecords = (records) => sortBy(reject(records, ({mileage}) => !mileage), 'mileage')
+var percent     = (n, p) => n * (p / 100)
+var bounds      = (n, variance) => n + variance
+var lower       = (mi) => bounds(mi, -variance(mi))
+var upper       = (mi) => bounds(mi, variance(mi))
+var cpm         = (records) => currentProjectedMileage(lastSeason(records), last(records))
+var lastSeason  = compose(partialRight(takeRight, 4), milesPerDaySet)
+var variance    = partialRight(percent, 1)
 
 describe('mini', function () {
   const ACTUAL_MILEAGE = 137854
   var records = sortRecords(rawMiniRecords)
 
-  it('should be within 1%', function () {
+  it('cpm should be within 1%', function () {
     expect(cpm(records), 'to be within', lower(ACTUAL_MILEAGE), upper(ACTUAL_MILEAGE))
   });
 });
@@ -42,7 +28,7 @@ describe('guzzi', function () {
   const ACTUAL_MILEAGE = 7554
   var records = sortRecords(rawGuzziRecords)
 
-  it('should be within 1%', function () {
+  it('cpm should be within 1%', function () {
     expect(cpm(records), 'to be within', lower(ACTUAL_MILEAGE), upper(ACTUAL_MILEAGE))
   });
 });
@@ -51,7 +37,7 @@ describe('suzuki', function () {
   const ACTUAL_MILEAGE = 4413
   var records = sortRecords(rawSuzukiRecords)
 
-  it('should be within 1%', function () {
+  it('cpm should be within 1%', function () {
     expect(cpm(records), 'to be within', lower(ACTUAL_MILEAGE), upper(ACTUAL_MILEAGE))
   });
 });
